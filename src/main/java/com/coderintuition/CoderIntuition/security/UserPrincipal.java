a59ad4dd61b627675a4003cbf73437a4dbb275ad
David Zhang <davidhqr@gmail.com>
2020-12-09 15:23:46 -0800

    fixes

--- src/main/java/com/coderintuition/CoderIntuition/security/UserPrincipal.java
+++ src/main/java/com/coderintuition/CoderIntuition/security/UserPrincipal.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class UserPrincipal implements OAuth2User, UserDetails {
     private Long id;
@@ -26,8 +27,9 @@ public class UserPrincipal implements OAuth2User, UserDetails {
     }
 
     public static UserPrincipal create(User user) {
-        List<GrantedAuthority> authorities = Collections.
-                singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
 
         return new UserPrincipal(
                 user.getId(),
