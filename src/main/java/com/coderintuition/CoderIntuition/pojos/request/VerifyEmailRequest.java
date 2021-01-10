1ff3f1867f9775254c466bbb981727fe0e53863d
David Zhang <davidhqr@gmail.com>
2021-01-09 17:12:21 -0800

    Implement email verification (#19)

---
+++ src/main/java/com/coderintuition/CoderIntuition/pojos/request/VerifyEmailRequest.java
@@ -0,0 +1,8 @@
+package com.coderintuition.CoderIntuition.pojos.request;
+
+import lombok.Data;
+
+@Data
+public class VerifyEmailRequest {
+    private String uuid;
+}
--- src/main/java/com/coderintuition/CoderIntuition/repositories/UserRepository.java
+++ src/main/java/com/coderintuition/CoderIntuition/repositories/UserRepository.java
@@ -11,4 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
     Optional<User> findByUsername(String username);
+    Optional<User> findByUuid(String uuid);
 }
