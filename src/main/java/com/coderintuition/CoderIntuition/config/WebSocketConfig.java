de24621690abf34c84c9e77754136b93d9d2c564
David Zhang <david.zhang2@shopify.com>
2021-04-07 22:20:08 -0700

    Update stomp heartbeats

--- src/main/java/com/coderintuition/CoderIntuition/config/WebSocketConfig.java
+++ src/main/java/com/coderintuition/CoderIntuition/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.coderintuition.CoderIntuition.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,13 +14,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
-                .setAllowedOrigins("*")
-                .withSockJS();
+            .setAllowedOrigins("*")
+            .withSockJS();
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config){
-        config.enableSimpleBroker("/topic/", "/queue/");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        ThreadPoolTaskScheduler te = new ThreadPoolTaskScheduler();
+        te.setPoolSize(1);
+        te.setThreadNamePrefix("wss-heartbeat-thread-");
+        te.initialize();
+
+        config.enableSimpleBroker("/topic/", "/queue/")
+            .setHeartbeatValue(new long[]{10000, 10000})
+            .setTaskScheduler(te);
         config.setApplicationDestinationPrefixes("/app");
     }
 }
