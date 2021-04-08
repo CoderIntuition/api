af192c5ffff1978ce37b537b3746bee1a7f7cf89
David Zhang <david.zhang2@shopify.com>
2021-04-07 21:26:36 -0700

    Improve exception handler

--- src/main/java/com/coderintuition/CoderIntuition/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/coderintuition/CoderIntuition/exceptions/CustomExceptionHandler.java
@@ -2,10 +2,12 @@ package com.coderintuition.CoderIntuition.exceptions;
 
 import com.coderintuition.CoderIntuition.pojos.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -14,9 +16,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.persistence.RollbackException;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings({"unchecked", "rawtypes"})
@@ -25,29 +30,23 @@ import java.util.List;
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllExceptions(Exception ex) {
-        log.warn("Handled Exception, class={}, message={}, stackTrace={}", ex.getClass().toString(), ex.getMessage(), ex.getStackTrace());
+        log.warn("Unhandled exception, class={}, message={}, stackTrace={}", ex.getClass().toString(), ex.getMessage(), ex.getStackTrace());
         ex.printStackTrace();
-        List<String> details = new ArrayList<>();
-        details.add(ex.getLocalizedMessage());
-        ErrorResponse error = new ErrorResponse("Error", details);
+        ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
         return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public final ResponseEntity<Object> handleBadCredentials() {
         log.warn("Handled BadCredentialsException");
-        List<String> details = new ArrayList<>();
-        details.add("Invalid email or password");
-        ErrorResponse error = new ErrorResponse("Authentication Error", details);
+        ErrorResponse error = new ErrorResponse("Authentication Error", List.of("Invalid email or password"));
         return new ResponseEntity(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RecordNotFoundException.class)
     public final ResponseEntity<Object> handleRecordNotFound(RecordNotFoundException ex) {
         log.warn("Handled RecordNotFoundException");
-        List<String> details = new ArrayList<>();
-        details.add(ex.getLocalizedMessage());
-        ErrorResponse error = new ErrorResponse("Record Not Found", details);
+        ErrorResponse error = new ErrorResponse("Record Not Found", List.of(ex.getLocalizedMessage()));
         return new ResponseEntity(error, HttpStatus.NOT_FOUND);
     }
 
@@ -65,7 +64,34 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     public final ResponseEntity<Object> handleBadRequest(BadRequestException ex) {
         log.warn("Handled BadRequestException");
-        ErrorResponse error = new ErrorResponse(ex.getLocalizedMessage(), new ArrayList<>());
+        ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
+        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public final ResponseEntity<Object> handleSQLIntegrityConstraintViolation(SQLIntegrityConstraintViolationException ex) {
+        log.warn("Handled SQLIntegrityConstraintViolationException");
+        ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
+        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TransactionSystemException.class)
+    public final ResponseEntity<Object> handleTransactionSystem(TransactionSystemException ex) {
+        if (ex.getRootCause() instanceof ConstraintViolationException) {
+            return handleConstraintViolation((ConstraintViolationException) ex.getRootCause());
+        }
+
+        ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
+        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public final ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        if (ex.getRootCause() instanceof SQLIntegrityConstraintViolationException) {
+            return handleSQLIntegrityConstraintViolation((SQLIntegrityConstraintViolationException) ex.getRootCause());
+        }
+
+        ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
         return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
     }
 
@@ -76,7 +102,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             details.add(error.getField() + " " + error.getDefaultMessage());
         }
-        ErrorResponse error = new ErrorResponse("Validation Failed", details);
+        ErrorResponse error = new ErrorResponse("Validation Error", details);
         return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
     }
 }
