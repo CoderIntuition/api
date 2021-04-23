61f99bc03cd07323925ac0b1c8a0f0ab9e33c710
David Zhang <david.zhang2@shopify.com>
2021-04-23 00:22:28 -0700

    Update exception handler

--- src/main/java/com/coderintuition/CoderIntuition/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/coderintuition/CoderIntuition/exceptions/CustomExceptionHandler.java
@@ -2,6 +2,7 @@ package com.coderintuition.CoderIntuition.exceptions;
 
 import com.coderintuition.CoderIntuition.pojos.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.awaitility.core.ConditionTimeoutException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,18 +11,15 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.transaction.TransactionSystemException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.persistence.RollbackException;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings({"unchecked", "rawtypes"})
@@ -30,29 +28,28 @@ import java.util.List;
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllExceptions(Exception ex) {
-        log.warn("Unhandled exception, class={}, message={}, stackTrace={}", ex.getClass().toString(), ex.getMessage(), ex.getStackTrace());
-        ex.printStackTrace();
+        log.error("Unhandled exception, class={}, message={}, stackTrace={}", ex.getClass().toString(), ex.getMessage(), ex.getStackTrace());
         ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
         return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
-    public final ResponseEntity<Object> handleBadCredentials() {
-        log.warn("Handled BadCredentialsException");
+    public final ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex) {
+        log.info("Handled BadCredentialsException, message={}", ex.getLocalizedMessage());
         ErrorResponse error = new ErrorResponse("Authentication Error", List.of("Invalid email or password"));
         return new ResponseEntity(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RecordNotFoundException.class)
-    public final ResponseEntity<Object> handleRecordNotFound(RecordNotFoundException ex) {
-        log.warn("Handled RecordNotFoundException");
+    public final ResponseEntity<Object> handleRecordNotFoundException(RecordNotFoundException ex) {
+        log.info("Handled RecordNotFoundException, message={}", ex.getLocalizedMessage());
         ErrorResponse error = new ErrorResponse("Record Not Found", List.of(ex.getLocalizedMessage()));
         return new ResponseEntity(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public final ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException ex) {
-        log.warn("Handled ConstraintViolationException");
+    public final ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
+        log.info("Handled ConstraintViolationException, message={}", ex.getLocalizedMessage());
         List<String> details = new ArrayList<>();
         for (ConstraintViolation violation : ex.getConstraintViolations()) {
             details.add(violation.getPropertyPath() + " " + violation.getMessage());
@@ -62,30 +59,31 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public final ResponseEntity<Object> handleBadRequest(BadRequestException ex) {
-        log.warn("Handled BadRequestException");
+    public final ResponseEntity<Object> handleBadRequestException(BadRequestException ex) {
+        log.info("Handled BadRequestException, message={}", ex.getLocalizedMessage());
         ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
         return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InternalServerException.class)
     public final ResponseEntity<Object> handleInternalServerException(InternalServerException ex) {
-        log.warn("Handled InternalServerException");
+        log.error("Handled InternalServerException, message={}, stackTrace={}", ex.getMessage(), ex.getStackTrace());
         ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
         return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public final ResponseEntity<Object> handleSQLIntegrityConstraintViolation(SQLIntegrityConstraintViolationException ex) {
-        log.warn("Handled SQLIntegrityConstraintViolationException");
+    public final ResponseEntity<Object> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException ex) {
+        log.info("Handled SQLIntegrityConstraintViolationException, message={}", ex.getLocalizedMessage());
         ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
         return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(TransactionSystemException.class)
-    public final ResponseEntity<Object> handleTransactionSystem(TransactionSystemException ex) {
+    public final ResponseEntity<Object> handleTransactionSystemException(TransactionSystemException ex) {
+        log.info("Handled TransactionSystemException, message={}", ex.getLocalizedMessage());
         if (ex.getRootCause() instanceof ConstraintViolationException) {
-            return handleConstraintViolation((ConstraintViolationException) ex.getRootCause());
+            return handleConstraintViolationException((ConstraintViolationException) ex.getRootCause());
         }
 
         ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
@@ -93,18 +91,27 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public final ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+    public final ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        log.info("Handled DataIntegrityViolation, message={}", ex.getLocalizedMessage());
         if (ex.getRootCause() instanceof SQLIntegrityConstraintViolationException) {
-            return handleSQLIntegrityConstraintViolation((SQLIntegrityConstraintViolationException) ex.getRootCause());
+            return handleSQLIntegrityConstraintViolationException((SQLIntegrityConstraintViolationException) ex.getRootCause());
         }
 
         ErrorResponse error = new ErrorResponse("Error", List.of(ex.getLocalizedMessage()));
         return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(ConditionTimeoutException.class)
+    public final ResponseEntity<Object> handleConditionTimeoutException(ConditionTimeoutException ex) {
+        log.warn("Handled ConditionTimeoutException, message={}, stackTrace={}", ex.getMessage(), ex.getStackTrace());
+        ErrorResponse error = new ErrorResponse("Timeout", List.of("Please try again or report this issue."));
+        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        log.warn("Handled MethodArgumentNotValidException");
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.warn("Handled MethodArgumentNotValidException, message={}", ex.getLocalizedMessage());
         List<String> details = new ArrayList<>();
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             details.add(error.getField() + " " + error.getDefaultMessage());
