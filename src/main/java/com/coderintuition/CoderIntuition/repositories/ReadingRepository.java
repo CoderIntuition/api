838a0a1f990ce97d76bd3dfd82461238aaa71044
David Zhang <david.zhang2@shopify.com>
2021-04-27 16:59:48 -0700

    Sort readings in reverse id order for CMS

--- src/main/java/com/coderintuition/CoderIntuition/repositories/ReadingRepository.java
+++ src/main/java/com/coderintuition/CoderIntuition/repositories/ReadingRepository.java
@@ -1,6 +1,9 @@
 package com.coderintuition.CoderIntuition.repositories;
 
 import com.coderintuition.CoderIntuition.models.Reading;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,6 +14,9 @@ import java.util.Optional;
 @Repository
 public interface ReadingRepository extends JpaRepository<Reading, Long> {
     Optional<Reading> findByUrlName(String urlName);
+
+    @Query("SELECT r FROM Reading r ORDER BY r.id DESC")
+    Page<Reading> findAll(@NotNull Pageable pageable);
 
     @Query("SELECT r.urlName FROM Reading r WHERE r.plusOnly=FALSE")
     List<String> findAllPublicUrlNames();
